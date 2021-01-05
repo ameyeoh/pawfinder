a0797b70a307c6a306f42a8e35a788c7d533d1ee
Amelia Yeoh <ameyjm@gmail.com>
2021-01-04 16:31:37 -0800

    Set up for deployment

--- my-app/server/index.js
+++ my-app/server/index.js
@@ -12,6 +12,12 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/api/dogs', controllers.getDogs);
 app.post('/api/dogs', controllers.addDog);
 
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+  });
+}
+
 app.listen(port, () => {
   console.log(`Server listening at http://localhost:${port}`);
 });
